8cb87bd9512e059eb10496df8a826aa6974c08ef
yd_per <yemaneberhane212@gmail.com>
2024-10-14 23:38:10 -0400

    src folder is commited

---
+++ src/main/java/com/githubcommandspractice/testingautomation/TestingAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.githubcommandspractice.testingautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/githubcommandspractice/testingautomation/TestingAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubcommandspractice.testingautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
